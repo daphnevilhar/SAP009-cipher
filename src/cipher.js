ccc5a338b9528246129baa682df2bd22912438fe
daphnevilhar <vilhardaphne@gmail.com>
2023-01-17 14:11:10 -0300

    Início do projeto

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,9 @@
+
 const cipher = {
-  // ...
+//cipher.encode(offset, string)
+//cipher.decode(offset, string)
 };
 
 export default cipher;
+
+let values = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+var caixaDeTexto1 = "";
+    function resultado(){
+        caixaDeTexto1 = document.getElementById("encryptBox").value;
+        document.getElementById("answer").innerHTML = caixaDeTexto1
+    }
+
+    function logar(){
+    var login = document.getElementById("login").value;
+    var senha = document.getElementById("senha").value;
+
+    if(login == "admin" && senha == "admin"){
+        alert('Sucesso');
+    }else{
+        alert('Usuário ou senha incorretos');
+    }
+}
