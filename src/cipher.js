5c2ab4dc64c34a849e934c15c1f8487328d63522
daphnevilhar <vilhardaphne@gmail.com>
2023-01-20 17:23:25 -0300

    Início do projeto

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,7 @@
 
 const cipher = {
-//cipher.encode(offset, string)
-//cipher.decode(offset, string)
+
 };
 
 export default cipher;
 
-let values = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
--- src/index.js
+++ src/index.js
@@ -2,19 +2,36 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-var caixaDeTexto1 = "";
-    function resultado(){
-        caixaDeTexto1 = document.getElementById("encryptBox").value;
-        document.getElementById("answer").innerHTML = caixaDeTexto1
+function criptografar(){
+    var input = document.getElementById("textBox1").value.toUpperCase()
+    var resultado = ""
+
+    for(var i = 0 ; i < input.length ; i++){  //Vai passar lendo cada caractere do input
+    
+    var posicaoDaLetra = input.charCodeAt(i)-64;//Identifica qual letra é o alfabeto
+    var deslocamento = (posicaoDaLetra + key) % 26; //Deslocamento mantendo dentro do alfabeto
+    
+    deslocamento = deslocamento == 0 ? 26 : deslocamento; // MOD retornar 0 caso o resuktado seja 26
+    resultado = value[deslocamento-1]; //Faz -1 porque a letra 1 A esta no indicice 0 do teu array
+
     }
 
-    function logar(){
+    document.getElementById("textBox2").innerHTML = resultado
+
+}
+document.getElementById("entrar").addEventListener("click", logar)
+
+function logar(e){
+    e.preventDefault()
+    console.log("oi")
     var login = document.getElementById("login").value;
     var senha = document.getElementById("senha").value;
 
     if(login == "admin" && senha == "admin"){
-        alert('Sucesso');
+        alert("Sucesso");
+        location.href = "index2.html";
     }else{
-        alert('Usuário ou senha incorretos');
+        alert("Usuario ou senha incorretos");
     }
+
 }
